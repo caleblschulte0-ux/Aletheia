961f43ecc5531492125d6940cf297814b5bccaef
caleblschulte0-ux <caleblschulte0@gmail.com>
2026-08-30 16:31:26 -0500

    [CHATGPT STAGING] export texting proposal and Phone Link probe

--- staging/jarvis_gap/__init__.py
+++ staging/jarvis_gap/__init__.py
@@ -4,7 +4,9 @@ from .camera_question import CameraQuestionPipeline
 from .desktop_context import DesktopContextObservation, WindowsContextBackend, capture
 from .mobile_sensors import EphemeralSensorBuffer, ImageObservation, validate_location
 from .ollama_vision import OllamaVisionBackend, OllamaVisionConfig
+from .phonelink_messages_probe import probe as probe_phonelink_messages
 from .sensor_requests import SensorCapture, SensorTicketStore
+from .texting import TextDraft, prepare as prepare_text
 from .vision import VisionAnswer, VisionReasoner
 from .visual_fallback import VisualTarget, VisualTargetPlanner
 
@@ -18,8 +20,11 @@ __all__ = [
     "validate_location",
     "OllamaVisionBackend",
     "OllamaVisionConfig",
+    "probe_phonelink_messages",
     "SensorCapture",
     "SensorTicketStore",
+    "TextDraft",
+    "prepare_text",
     "VisionAnswer",
     "VisionReasoner",
     "VisualTarget",
